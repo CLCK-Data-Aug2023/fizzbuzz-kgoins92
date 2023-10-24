294fc76728df73878906308297ae7f8a1d8cd916
Kaitlyn Goins <143042109+kgoins92@users.noreply.github.com>
2023-10-24 01:03:44 -0400

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,11 +1,1 @@
-for fizzbuzz in range(1, 101):
-    if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
-        print("Fizzbuzz")
-        continue
-    elif fizzbuzz % 3 == 0:
-        print("Fizz")
-        continue
-    elif fizzbuzz % 5 == 0:
-        print("Buzz")
-        continue
-    print(fizzbuzz)
+
